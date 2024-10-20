2adf8c292e41e4f79593c883b5ef9ce93f3053c4
namanshah01 <nashah2001@gmail.com>
2024-10-19 18:22:37 -0700

    minor updates

--- server.py
+++ server.py
@@ -129,5 +129,5 @@ def update_requests():
     else:
         return jsonify({"success": False, "message": "API key not found"}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
